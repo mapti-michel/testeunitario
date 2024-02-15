a9967fa07adcaeefb02ce35db0b631051e26a629
Michel Pereira <michel.a.pereira@accenture.com>
2024-02-15 11:44:36 -0300

    Update files

---
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,35 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+//import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+
+    @Test
+    public void test() {
+
+        // cenário
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuário 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+
+        // ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        // verificação
+        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+
+
+
+}
